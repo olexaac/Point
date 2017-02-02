c4360c7ee6172c49fd4cf9456ec9d26fad083fad
olexaac <arbeidnsk@gmail.com>
2017-02-02 18:41:36 +0600

    Создана функция вычисления и сделан запускаемый класс

--- homework2/src/main/java/ru/stqa/pft/homework2/Point.java
+++ homework2/src/main/java/ru/stqa/pft/homework2/Point.java
@@ -1,12 +1,24 @@
 package ru.stqa.pft.homework2;
 
-class Point {
+public class Point {
+  double x;
+  double y;
 
-	int x,y;
-	public static void main(String args[]) {
-		Point p = new Point();
-		p.x = 10;
-		p.y = 20;
-	System.out.println("x = " + p.x + " y = " + p.y);
-	}
+  Point(double x, double y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  public static double distance(Point p1, Point p2) {
+    return Math.sqrt((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y));
+  }
+
+  public static void main(String[] args) {
+    Point p1;
+    Point p2;
+    p1 = new Point(3.0, 5.0);
+    p2 = new Point(6.0, 9.0);
+    System.out.print(Point.distance(p1, p2));
+  }
 }
+
