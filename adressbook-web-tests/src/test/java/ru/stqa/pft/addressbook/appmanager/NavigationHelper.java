540b5c82790df622ca4b6ab7b7e5eb2566e5adbc
olexaac <arbeidnsk@gmail.com>
2017-03-21 20:25:10 +0600

    Создан общий базовый класс HelperBase для вспомогательных классов

--- adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -6,14 +6,14 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 /**
  * Created by Админ on 10.03.2017.
  */
-public class NavigationHelper {
+public class NavigationHelper extends HelperBase {
   private FirefoxDriver wd;
 
   public NavigationHelper(FirefoxDriver wd) {
-    this.wd = wd;
+    super(wd);
   }
 
   public void gotoGroupPage() {
-    wd.findElement(By.linkText("groups")).click();
+    click(By.linkText("groups"));
   }
 }
---
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/SessionHelper.java
@@ -0,0 +1,20 @@
+package ru.stqa.pft.addressbook.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * Created by Админ on 10.03.2017.
+ */
+public class SessionHelper extends HelperBase {
+
+  public SessionHelper(FirefoxDriver wd) {
+    super(wd);
+  }
+
+  public void login(String username, String password) {
+    type(By.name("user"), username);
+    type(By.name("pass"), password);
+    click(By.xpath("//form[@id='LoginForm']/input[3]"));
+  }
+}
