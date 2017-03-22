27f7cf2886043bd5d5ca768f94d707884f65408c
olexaac <arbeidnsk@gmail.com>
2017-03-22 16:33:22 +0600

    Добавлен сценарий модификации группы

--- adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -30,4 +30,16 @@ public class GroupHelper extends HelperBase {
   public void initGroupCreation() {
     click(By.name("new"));
   }
+
+  public void selectGroup() {
+    click(By.name("selected[]"));
+  }
+
+  public void initGroupModification() {
+    click(By.name("edit"));
+  }
+
+  public void submitGroupModification() {
+    click(By.name("update"));
+  }
 }
